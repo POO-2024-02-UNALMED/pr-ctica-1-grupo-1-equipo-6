1897ca69ccc7789b879913b3a541a3ba359405c1
AlPerBara <alperezba@unal.edu.co>
2025-01-19 11:13:53 -0500

    Add files via upload

--- src/baseDatos/cargarObjetos.java
+++ src/baseDatos/cargarObjetos.java
@@ -5,15 +5,15 @@ package baseDatos;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.Serializable;
 import java.util.ArrayList;
 
 import gestorAplicacion.reservacionHotel.Destino;
 
-public class cargarObjetos implements Serializable{
+public class cargarObjetos{
 
     private static final String DIRECCION_BBDD="src/baseDatos/temp/"; //En esta ruta es donde se almacenan los archivos
 
+    @SuppressWarnings("unchecked")
     public static ArrayList<Destino> destinos(){ //Este método carga destinos
 
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(DIRECCION_BBDD+"destinos.txt"))) {
--- src/baseDatos/guardarObjetos.java
+++ src/baseDatos/guardarObjetos.java
@@ -1,5 +1,28 @@
+//Inicialmente codificado por Alejandro Pérez Barrera
+
 package baseDatos;
 
-public class guardarObjetos {
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import gestorAplicacion.reservacionHotel.Destino;
+
+public class guardarObjetos{
     
+    private static final String DIRECCION_BBDD="src/baseDatos/temp/"; //En esta ruta es donde se almacenan los archivos
+
+    public static boolean guardar(ArrayList<Destino> destinos){//Este código de este método se encarga de guardar los datos
+        //Solo es sobrecargarlo con el tipo de datos necesario, y cambiar la ruta donde se guardan las cosas
+        //El retorno en forma de boolean es para verificar que si se hayan guardado las cosas
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(DIRECCION_BBDD+"destinos.txt"))){ 
+            oos.writeObject(destinos);
+            return true;
+        }
+        catch(IOException e){
+            return false;
+        }
+    }
+
 }
